29bfc0a1753fcb07a18ca8d17bc73cbe178e2f04
AGL_Hà Quang Anh (アイン) <ha_quang_anh@allgrow-labo.jp>
2022-12-09 15:03:22 +0700

    check validate html css

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,20 +1,29 @@
-// align link1 elem: logo, link 1, breadcrumb header home 
+// align link1 elem: logo, link 1, breadcrumb header home
+// ***************************************************************
 var logo = document.querySelector(".c-header__img");
+var home = document.querySelector(".c-header__1");
+
 var link1 = document.querySelector(".c-header__2");
+var link2 = document.querySelector(".c-header__3");
+
 var headerBreadcrumbHome = document.querySelector(".c-header__home");
 // var headerBreadcrumbHomeLogo = document.querySelector(".c-header__home::before");
 
 if (document.title == "Link 1") {
-  logo.style.transform = "translate(0, 1px)";
-  link1.style.transform = "translate(3px, 1px)";
-  headerBreadcrumbHome.style.transform = "translate(-1px, 2px)";
-  headerBreadcrumbHome.style.fontWeight = "500";
+  // logo.style.transform = "translate(0, 1px)";
+  link1.style.transform = "translate(3px, 0px)";
+  // headerBreadcrumbHome.style.transform = "translate(-1px, 2px)";
+  // headerBreadcrumbHome.style.fontWeight = "500";
+  // headerBreadcrumbHomeLogo.style.transform = "translate()";
+}
+
+if (document.title == "Link 2") {
+  home.style.transform = "translate(-2px, 0px);";
   // headerBreadcrumbHomeLogo.style.transform = "translate()";
 }
 
 // Đường link in đậm khi truy cập trang nhất định
-var home = document.querySelector(".c-header__1");
-var link2 = document.querySelector(".c-header__3");
+// ***************************************************************
 
 if (document.title == "Home page") {
   home.classList.add("c-header__link--bold");
